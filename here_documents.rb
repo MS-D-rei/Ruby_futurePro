170d73fc4152c2af298613dcdc549c777323993b
MS-D-rei <msmdmission1111@gmail.com>
2020-12-13 22:30:17 +0900

    here document

---
+++ here_documents.rb
@@ -0,0 +1,18 @@
+puts <<GROCERY_LIST
+Grocery list
+----------------
+1. Salad mix.
+2. Strawberries.*
+3. Cereal.
+4. Milk.*
+
+* Organic
+GROCERY_LIST
+
+lang = 'Ruby'
+lang.prepend(<<Programming_Language)
+Java
+PHP
+Python
+Programming_Language
+puts lang
