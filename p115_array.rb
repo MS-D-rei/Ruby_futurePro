e2ed781adad231bb573d36399a6c2cc57a541d6a
MS-D-rei <msmdmission1111@gmail.com>
2020-12-28 16:54:31 +0900

    array fundamental  Please enter the commit message for your changes. Lines starting

--- p115_array.rb
+++ p115_array.rb
@@ -1,1 +1,22 @@
-p115_array.rb
+# ruby p115_array.rb
+a = [1, 2, 3, 4, 5]
+p a[1, 3] # => [2, 3, 4]
+p a.values_at(0, 2, 4) # => [1, 3, 5] array.values_at[index, index, ...] => [array[index], array[index], ...]
+p a[a.size - 1] # => 5
+p a.last # => 5
+p a.last(2) # => [4, 5]
+p a.first(2) # => [1, 2]
+
+a[1, 3] = 100
+p a # =>[1, 100, 5]
+a.delete(100)
+p a.push(6, 7) # => [1, 5, 6, 7]
+p a << 8 # => [1, 5, 6, 7, 8]
+
+# set
+b = [1, 2, 3]
+c = [3, 4, 5]
+p b | c # => [1, 2, 3, 4, 5]
+p b - c # => [1, 2]
+p b & c # => [3]
+
