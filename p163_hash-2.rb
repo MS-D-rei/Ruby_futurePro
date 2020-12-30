c6e9440f0d9f92904407fccb77b2d0fe8c0a4910
MS-D-rei <msmdmission1111@gmail.com>
2020-12-30 17:51:51 +0900

    how to use **arg in a hash

--- p163_hash-2.rb
+++ p163_hash-2.rb
@@ -1,4 +1,21 @@
 # ruby p163_hash-2.rb
 
 h = { us: 'dollar', india: 'rupee' }
-p { japan: 'yen', **h }
+a = { japan: 'yen', **h }
+p a # => {:japan=>"yen", :us=>"dollar", :india=>"rupee"}
+
+def buy_burger(menu, drink:, potato:, **others) # other args are put in **arg as hash
+    puts menu
+    if drink
+        puts drink
+    end
+    if potato
+        puts potato
+    end
+    if others
+        puts others
+    end
+end
+
+buy_burger('fish', drink: 'tea', potato: 'potato s', salad: 'salad s', chicken: '10 pcs')
+# => fish, tea, potato s, {:salad=>"salad s", :chicken=>"10 pcs"}
