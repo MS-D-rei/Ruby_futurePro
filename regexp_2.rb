fbac48323366f6d6f8fd8b5fa2f79fce048a17c7
MS-D-rei <msmdmission1111@gmail.com>
2020-12-31 16:36:39 +0900

    regexp ()matches, ?: except from (), * 0 or 1 or more, \w a-z0-9A-Z , ˆexcept

---
+++ regexp_2.rb
@@ -0,0 +1,17 @@
+# ruby regexp_2.rb
+
+text = <<TEXT
+<select name="game_console">
+<option value="none"></option>
+<option value="pc" selected>PC</option>
+<option value="ps5">PS5</option>
+<option value="switch">Switch</option>
+TEXT
+
+p text.scan(/<option value="[a-z0-9_]+">.+<\/option>/) # => ["<option value=\"ps5\">PS5</option>", "<option value=\"switch\">Switch</option>"]
+p text.scan(/<option value="([a-z0-9_]+)">(.+)<\/option>/) # => () make matches, [["ps5", "PS5"], ["switch", "Switch"]]
+p text.scan(/<option value="([a-z0-9_]+)">(.*)<\/option>/) # => * means 0 or 1 or more, [["none", ""], ["ps5", "PS5"], ["switch", "Switch"]]
+p text.scan(/<option value="([a-z0-9_]+)"( selected)?>(.*)<\/option>/) # => [["none", nil, ""], ["pc", " selected", "PC"], ["ps5", nil, "PS5"], ["switch", nil, "Switch"]]
+p text.scan(/<option value="([a-z0-9_]+)"(?: selected)?>(.*)<\/option>/) # => [["none", ""], ["pc", "PC"], ["ps5", "PS5"], ["switch", "Switch"]]
+p text.scan(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/) # \w includes a-z, 0-9, A-Z
+p text.scan(/<option value="(\w+)"(?: selected)?>([ˆ<]*)<\/option>/) # how to input ˆ => option + i, ˆ< means except for <
