92cf45ac306438ab6b74495f4de6ae182fe6f9d5
MS-D-rei <msmdmission1111@gmail.com>
2021-04-20 20:42:09 +0900

    Can't call private method with receiver

---
+++ p247_private.rb
@@ -0,0 +1,17 @@
+# ruby p247_private.rb
+
+class User
+    def hello
+        "Hello, I am #{name}"
+    end
+
+    # can't call private method without receiver
+    private
+
+    def name
+        'Alice'
+    end
+end
+
+user = User.new
+p user.hello
