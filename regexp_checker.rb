370bd7fa7a9a1fc2f01def1cc48183276c4487c7
MS-D-rei <msmdmission1111@gmail.com>
2021-05-30 21:16:17 +0900

    text = gets.chomp

---
+++ regexp_checker.rb
@@ -0,0 +1,20 @@
+# ruby regexp_checker.rb
+
+print 'Text?: '
+text = gets.chomp
+
+begin
+    print "Pattern?: "
+    pattern = gets.chomp
+    regexp = Regexp.new(pattern)
+rescue RegexpError => e
+    puts "Invalid pattern: #{e.message}"
+    retry # => control + c => exit
+end
+
+matches = text.scan(regexp)
+if matches.size > 0
+    puts "Matched: #{matches.join(', ')}"
+else
+    puts "Nothing matched"
+end
