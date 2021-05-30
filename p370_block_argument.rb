0ec9d759913aaea978dc5bd906d305eb86820c5e
MS-D-rei <msmdmission1111@gmail.com>
2021-05-30 22:26:36 +0900

    block argument $block

---
+++ p370_block_argument.rb
@@ -0,0 +1,28 @@
+# ruby p370_block_argument.rb
+
+def greeting(&block)
+    puts 'morning'
+    text = block.call('afternoon')
+    puts text
+    puts 'evening'
+end
+
+greeting do |example|
+    example * 2
+end
+
+def greeting_en(&block)
+    texts = ['morning', 'afternoon', 'evening']
+    greeting_common(texts, &block)
+end
+
+# need to put block argument at last
+def greeting_common(texts, &block)
+    puts texts[0]
+    puts block.call(texts[1])
+    puts texts[2]
+end
+
+greeting_en do |aaa|
+    aaa * 3
+end
