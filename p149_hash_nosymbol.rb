abd584ac4a23e4e151dc2f14297ab87148972dc3
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-11-18 15:40:48 +0900

    think hash is a collection of arrays

--- p149_hash_nosymbol.rb
+++ p149_hash_nosymbol.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # ruby p149_hash.rb
 
 currencies = { 'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee' }
@@ -5,11 +7,11 @@ currencies = { 'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee' }
 currencies['italy'] = 'euro'
 
 currencies.each do |key, value|
-    puts "#{key}: #{value}"
+  puts "#{key}: #{value}"
 end
 
 currencies.each do |key_value_array| # if set 1 args, put key and value in array
-    key = key_value_array[0]
-    value = key_value_array[1]
-    puts "#{key}: #{value}"
+  key = key_value_array[0]
+  value = key_value_array[1]
+  puts "#{key}: #{value}"
 end
