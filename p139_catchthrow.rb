67b6aa16371b5f13aebca5fa714399ec094fcbc4
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-11-18 12:03:25 +0900

    catch :symbol do done :symbol end

--- p139_catchthrow.rb
+++ p139_catchthrow.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 # ruby p139_catchthrow.rb
 
-fruits = ['apple', 'melon', 'orange']
+fruits = %w[apple melon orange]
 numbers = [1, 2, 3]
 
 catch :done do
-    fruits.shuffle.each do |fruit|
-        numbers.shuffle.each do |n|
-            puts "#{fruit}: #{n}"
-            if fruit == 'orange' && n == 3
-                throw :done
-            end
-        end
+  fruits.shuffle.each do |fruit|
+    numbers.shuffle.each do |n|
+      puts "#{fruit}: #{n}"
+      throw :done if fruit == 'orange' && n == 3
     end
+  end
 end
 
+# 'catch :symbol do throw :symbol end' can use without loop
+
 ret = catch :done do
-    throw :done, '123' # return the second argument
+  throw :done, '123' # return the second argument
 end
 puts ret
