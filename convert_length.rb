a9974d4273dd0eec850cf24c3e0acd459be9fcb8
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-11-18 16:05:36 +0900

    keyword arg is not symbol that's why UNITS[from], not [:from]

--- convert_length.rb
+++ convert_length.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 # ruby convert_length.rb
 
-UNITS = { m: 1.0, ft: 3.28, in: 39.37 }
+UNITS = { m: 1.0, ft: 3.28, in: 39.37 }.freeze
+
 def convert_length(length, from:, to:)
-    units = { m: 1.0, ft: 3.28, in: 39.37 }
-    (length / UNITS[from] * UNITS[to]).round(2)
+  (length / UNITS[from] * UNITS[to]).round(2)
 end
