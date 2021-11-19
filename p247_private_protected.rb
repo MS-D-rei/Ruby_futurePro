df2fbfc2092fb9eb0f91b79a923c057775b7652a
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-11-19 15:47:51 +0900

    private and protect

--- p247_private_protected.rb
+++ p247_private_protected.rb
@@ -1,50 +1,53 @@
+# frozen_string_literal: true
+
 # ruby p247_private_protected.rb
 
+# users
 class User
-    attr_reader :name
+  attr_reader :name
 
-    def initialize(name = "Maria", weight = 50)
-        @name = name
-        @weight = weight
-    end
+  def initialize(name = 'Maria', weight = 50)
+    @name = name
+    @weight = weight
+  end
 
-    def hello
-        "Hello, I am #{@name}"
-    end
+  def hello
+    "Hello, I am #{@name}"
+  end
 
-    def foo
-        'foo'
-    end
+  def foo
+    'foo'
+  end
 
-    def heavier_than?(other_user)
-        # can use other weight because it is not private method.
-        other_user.weight < @weight
-    end
+  def heavier_than?(other_user)
+    # can use other weight because it is not private method.
+    other_user.weight < @weight
+  end
 
-    # use protected method when we don't want to open but want to use it in class or sub class.
-    protected
+  # use protected method when we don't want to open but want to use it in class or sub class.
+  protected
 
-    def weight
-        @weight
-    end
+  attr_reader :weight
 
-    # can't call private method with receiver
-    private :foo # can add public method as private
+  # can't call private method with receiver
+  private :foo # can add public method as private
 
-    def name
-        'Alice'
-    end
+  def name
+    'Alice'
+  end
 end
 
+# VIP of users
 class VIP < User
-    # overwrite hello method
-    def hello
-        "Glad to meet you, Ms. #{name}"
-    end
-    def to_s
-        # can use private method of super class
-        "name: #{name}"
-    end
+  # overwrite hello method
+  def hello
+    "Glad to meet you, Ms. #{name}"
+  end
+
+  def to_s
+    # can use private method of super class
+    "name: #{name}"
+  end
 end
 
 user = User.new
@@ -53,8 +56,8 @@ p user.hello # => "Hello, I am Maria"
 p vip.hello # => "Glad to meet you, Ms. Alice"
 p vip.to_s # => "name: Alice"
 
-maria = User.new("Maria", 50)
-john = User.new("John", 70)
+maria = User.new('Maria', 50)
+john = User.new('John', 70)
 
 p maria.heavier_than?(john) # => false
 # p maria.weight
